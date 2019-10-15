139d7047377a7b41582c831c6093be296dacd579
Lauren Riddle <lari7132@gmail.com>
2019-10-15 10:35:21 -0500

    fixed the function for the Conjunction Function Exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
-console.log("Test Log!")
+// console.log("Test Log!")
 
 
 
 // C is for Cookie Exercise
 
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 
-for (let x = 1; x < cookies.length; x++) {
-    const currentCookie = cookies[x]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let x = 1; x < cookies.length; x++) {
+//     const currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+
+
+
+// Conjunction Function
+let conjoinedWord;
+const conjunction = function (firstWord, secondWord) {
+    conjoinedWord = `${firstWord} ${secondWord}`
 }
+
+conjunction("Master", "Card")
+
+console.log(conjoinedWord)
